bf1fd7882eff039fae91ef815845d51b58c39749
刘汇 <lemons.lhui@gmail.com>
2021-05-16 19:18:11 +0800

    fix:title

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -2,7 +2,7 @@
  * @Author: Lemon
  * @Date: 2021-05-16 01:05:10
  * @LastEditors: Lemon
- * @LastEditTime: 2021-05-16 02:48:03
+ * @LastEditTime: 2021-05-16 19:17:57
  * @FilePath: /august-blog-service-midway/src/lib/common.ts
  */
 
@@ -10,11 +10,9 @@ export const getTitle = (str: string) => {
   if (!str) {
     return ''
   }
-  let _str = str;
-  _str = _str.replace(/\<[^>]*\>(([^<])*)/g, function () {
-    let mark = '';
-    return arguments[1];
-  });
-  _str = _str.substring(0, str.length - 1);
-  return _str
+  const isMatch = str.match(/(<h[1-6][^>]*>)([^><]+)(<\/h[1-6]>)/)
+  if (!isMatch) {
+    return ''
+  }
+  return isMatch[2]
 };
